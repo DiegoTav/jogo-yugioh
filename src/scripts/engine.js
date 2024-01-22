19d72cf61572e31c8202735ea3baf582d649ab90
Diego Tavares <sdiego14599@gmail.com>
2024-01-21 22:25:44 -0300

    Criando os Estados da Engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,33 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-image"),
+        type: document.getElementById("card-image"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
+};
+
+
+
+// const players = {
+//     // player1: "player-cards",
+
+// }
+
+function init() {
+    // players.player1
+
+}
+
+init();
